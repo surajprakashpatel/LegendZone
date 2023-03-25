a747cbe4a79a0d813de09ce25a3ddfa4dd91b4cc
Suraj <13surajprakash13@gmail.com>
2023-03-25 18:19:22 +0530

    Splash and Welcome Activity Completed

--- app/src/main/java/com/srsoft/legendzone/ui/activity/SplashActivity.java
+++ app/src/main/java/com/srsoft/legendzone/ui/activity/SplashActivity.java
@@ -2,12 +2,19 @@ package com.srsoft.legendzone.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.srsoft.legendzone.R;
 import com.srsoft.legendzone.ui.common.BaseActivity;
 
@@ -18,6 +25,7 @@ public class SplashActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
 
         if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
             setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
@@ -30,5 +38,32 @@ public class SplashActivity extends BaseActivity {
             setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
+        initialization();
     }
+
+   private void initialization(){
+
+       FirebaseApp.initializeApp(this);
+       FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
+       firebaseAppCheck.installAppCheckProviderFactory(
+               PlayIntegrityAppCheckProviderFactory.getInstance());
+
+       FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+       new Handler().postDelayed(new Runnable() {
+           @Override
+           public void run() {
+               if(user!=null){
+                   Intent intent = new Intent(SplashActivity.this,WelcomeActivity.class);
+                   startActivity(intent);
+                   finish();
+               }else{
+                   Intent intent = new Intent(SplashActivity.this,WelcomeActivity.class);
+                   startActivity(intent);
+                   finish();
+               }
+           }
+       },2000);
+
+
+   }
 }
--- app/src/main/java/com/srsoft/legendzone/ui/activity/WelcomeActivity.java
+++ app/src/main/java/com/srsoft/legendzone/ui/activity/WelcomeActivity.java
@@ -1,19 +1,138 @@
 package com.srsoft.legendzone.ui.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.service.voice.VoiceInteractionSession;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.srsoft.legendzone.R;
 import com.srsoft.legendzone.databinding.ActivityWelcomeBinding;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     private ActivityWelcomeBinding binding;
+
+    private ViewPagerAdapter viewPagerAdapter;
+
+    private int images[] = {R.drawable.slider_img1, R.drawable.slider_img2, R.drawable.slider_img3};
+
+    private int headings[] = {R.string.heading1, R.string.heading2, R.string.heading3};
+
+    private int descriptions[] = {R.string.desc1, R.string.desc2, R.string.desc3};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityWelcomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        initialization();
+    }
+
+    private void initialization() {
+
+        binding.tvTitle.setText(headings[0]);
+        binding.tvDesc.setText(descriptions[0]);
+
+        viewPagerAdapter = new ViewPagerAdapter();
+        binding.viewPager.setAdapter(viewPagerAdapter);
+
+        binding.dotsIndicator.setViewPager(binding.viewPager);
+
+        binding.viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == images.length - 1) {
+                    binding.btnNext.setVisibility(View.GONE);
+                    binding.tvSkip.setVisibility(View.GONE);
+                    binding.btnStart.setVisibility(View.VISIBLE);
+                } else {
+                    binding.btnNext.setVisibility(View.VISIBLE);
+                    binding.tvSkip.setVisibility(View.VISIBLE);
+                    binding.btnStart.setVisibility(View.GONE);
+                }
+
+                binding.tvTitle.setText(headings[position]);
+                binding.tvDesc.setText(descriptions[position]);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        binding.btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int current = getItem(+1);
+                if (current < images.length) {
+                    // move to next screen
+                    binding.viewPager.setCurrentItem(current);
+                }
+            }
+        });
+
+        binding.btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, SplashActivity.class);
+                startActivity(intent);
+                finishAffinity();
+            }
+        });
+    }
+
+    private int getItem(int i) {
+        return binding.viewPager.getCurrentItem() + i;
+    }
+
+    public class ViewPagerAdapter extends PagerAdapter {
+        private LayoutInflater layoutInflater;
+        public ViewPagerAdapter() {
+        }
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(R.layout.welcome_slider_layout, container, false);
+            container.addView(view);
+
+            ImageView sliderImage = (ImageView) view.findViewById(R.id.sliderImage);
+
+            sliderImage.setImageResource(images[position]);
+
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return images.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object obj) {
+            return view == obj;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
     }
 }
